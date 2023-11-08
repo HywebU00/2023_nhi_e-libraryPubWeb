f573dc7370dda701c162bbda57c9e5939f7d870b
Miley <miley.ho@hyweb.com.tw>
2023-11-08 18:17:37 +0800

    first commit

---
+++ js/customize.js
@@ -0,0 +1,234 @@
+// -----  基本功能開關   ---------------------------------------------------
+window.addEventListener('load', () => {
+  topNav(); // 手機版顯示nav選單
+  //navSticky(); // 捲動時固定主選單
+  fatFooter(); // fatFooter是否要展開
+  tabFunction('.tabSet'); // tab功能
+  scrollTables('.tableWrapper'); // table捲動功能
+
+  mobileSearch();
+
+  tableAddDataAttributes({
+    elemClass: '.tableList', // 目標table
+    dataName: 'title', // tableList樣式 加上 data-title
+  });
+
+  // 全站字體
+  fontSize();
+
+  // 手風琴功能
+  accordionFunction({
+    accordion: '.accordion',
+    openFirst: false, // 預設先展開所有內容，使用無障礙遊走不再有手風琴效果，永遠展開內容(滑鼠點擊正常開合)
+    autoClose: true, // 點擊時自動關閉已展開的項目，若需要此功能需要關閉openFirst
+    duration: 200,
+    info: {
+      open: '展開', // 收合時顯示
+      close: '收合', // 展開時顯示
+    },
+  });
+});
+// -----  基本功能開關   ---------------------------------------------------
+
+// 自行加入的JS請寫在這裡
+(function () {
+  //cp輪播
+  const cpSwiper = new Swiper('.cpSlider .swiper', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: false,
+    // 切換點
+    pagination: {
+      el: '.cpSlider .swiperDots',
+      bulletElement: 'button',
+      clickable: true,
+    },
+    // 切換箭頭
+    navigation: {
+      nextEl: '.cpSlider .nextSlider', //自行設定樣式
+      prevEl: '.cpSlider .prevSlider', //自行設定樣式
+      disabledClass: 'swiperArrow-disabled', //不可點選樣式
+    },
+    breakpoints: {
+      100: {
+        slidesPerView: 2,
+      },
+      767: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  //首頁video輪播
+  const videoSwiper = new Swiper('.videoSlider .swiper', {
+    slidesPerView: 1.6,
+    //centeredSlides: true,
+    spaceBetween: -40,
+    loop: true,
+    // 切換點
+    pagination: {
+      el: '.videoSlider .swiperDots',
+      bulletElement: 'button',
+      clickable: true,
+    },
+    // 切換箭頭
+    navigation: {
+      nextEl: '.videoSlider .nextSlider', //自行設定樣式
+      prevEl: '.videoSlider .prevSlider', //自行設定樣式
+      disabledClass: 'swiperArrow-disabled', //不可點選樣式
+    },
+    breakpoints: {
+      100: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 1.6,
+      },
+    },
+  });
+
+  //首頁video輪播
+  const postSwiper = new Swiper('.postSlider .swiper', {
+    slidesPerView: 2,
+    //centeredSlides: true,
+    spaceBetween: 22,
+    loop: true,
+    // 切換點
+    pagination: {
+      el: '.postSlider .swiperDots',
+      bulletElement: 'button',
+      clickable: true,
+    },
+    // 切換箭頭
+    navigation: {
+      nextEl: '.postSlider .nextSlider', //自行設定樣式
+      prevEl: '.postSlider .prevSlider', //自行設定樣式
+      disabledClass: 'swiperArrow-disabled', //不可點選樣式
+    },
+    breakpoints: {
+      100: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 2,
+      },
+    },
+  });
+
+  //大圖輪播
+  let mpSliderItem = document.querySelectorAll('.mpSlider .swiper-slide');
+  let mpSliderPagination = [];
+  mpSliderItem.forEach((item, index) => {
+    mpSliderPagination.push(item.dataset.title);
+  });
+  const mpSlider = new Swiper('.mpSlider .swiper', {
+    slidesPerView: 1,
+    loop: false,
+    // 切換點
+    pagination: {
+      el: '.mpSlider .swiperDots',
+      bulletElement: 'button',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return `<button class="${className} noFonts" aria-label="${mpSliderPagination[index]}">${mpSliderPagination[index]}</button>`;
+      },
+    },
+    // 切換箭頭
+    navigation: {
+      nextEl: '.mpSlider .nextSlider', //自行設定樣式
+      prevEl: '.mpSlider .prevSlider', //自行設定樣式
+      disabledClass: 'swiperArrow-disabled', //不可點選樣式
+    },
+  });
+
+  //廣告輪播
+  // const adSwiper = new Swiper('.adSlider .swiper', {
+  //   slidesPerView: 5,
+  //   spaceBetween: 30,
+  //   loop: false,
+  //   // 切換點
+  //   pagination: {
+  //     el: '.adSlider .swiperDots',
+  //     bulletElement: 'button',
+  //     clickable: true,
+  //   },
+  //   // 切換箭頭
+  //   navigation: {
+  //     nextEl: '.adSlider .nextSlider', //自行設定樣式
+  //     prevEl: '.adSlider .prevSlider', //自行設定樣式
+  //     disabledClass: '.adSlider swiperArrow-disabled', //不可點選樣式
+  //   },
+  //   breakpoints: {
+  //     100: {
+  //       slidesPerView: 2,
+  //     },
+  //     767: {
+  //       slidesPerView: 3,
+  //     },
+  //     1000: {
+  //       slidesPerView: 4,
+  //     },
+  //   },
+  // });
+
+  //跑馬燈
+  const marqueeSwiper = new Swiper('.marquee .swiper', {
+    direction: 'vertical',
+    // 切換點
+    // 切換箭頭
+    navigation: {
+      nextEl: '.marquee .nextSlider', //自行設定樣式
+      prevEl: '.marquee .prevSlider', //自行設定樣式
+      disabledClass: '.marquee marquee-arrow-disabled', //不可點選樣式
+    },
+  });
+
+  //cp_photo
+  const navSlider = new Swiper('.navSlider .swiper', {
+    lazy: true, // lazy load
+    preloadImages: false, // 多筆設定lazy時須設定
+    centeredSlides: false, // 多筆設定lazy時須設定
+    slidesPerView: 4,
+    // watchSlidesProgress: true,
+    navigation: {
+      nextEl: '.navSlider .nextSlider', //下一張class，無障礙設定關係需要增加.nextSlider
+      prevEl: '.navSlider .prevSlider', //前一張class，無障礙設定關係需要增加.prevSlider
+      disabledClass: 'swiperArrow-disabled', //不可點選樣式
+    },
+    breakpoints: {
+      100: {
+        slidesPerView: 2,
+      },
+      767: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  const sliderFor = new Swiper('.sliderFor .swiper', {
+    slidesPerView: 1, //顯示張數
+    effect: 'fade', //淡入
+    fadeEffect: {
+      crossFade: true, //上一張淡出，false上一張不淡出，下一張疊在上方
+    },
+    pagination: {
+      el: '.sliderFor .pagination',
+      type: 'fraction', //顯示分頁
+    },
+    lazy: true,
+    thumbs: {
+      swiper: navSlider, //設定指向到哪個swiper，使用另一個設定的參數
+    },
+  });
+  var svgElement = document.querySelector('.mySvg');
+
+  if (svgElement) {
+    var pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    pathElement.setAttribute('d', svgElement.innerHTML);
+    svgElement.appendChild(pathElement);
+    svgElement.parentNode.removeChild(svgElement);
+  }
+})();
